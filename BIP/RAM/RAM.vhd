d5e1188fda9e8ee8880e590302f3035680fc9fc4
CodeWracker <dev.rodrigofs@gmail.com>
2022-06-30 22:50:56 -0300

    arrumei a ram pra bater com o que eu fiz no logisim

--- BIP/RAM/RAM.vhd
+++ BIP/RAM/RAM.vhd
@@ -9,32 +9,51 @@ entity RAM is
 		p_add_width : 		integer := 12
 	);
 	Port(
+		-- clock
 		i_clk 		: in 	std_logic;
+		
+		-- entrada
 		i_data 		: in 	std_logic_vector((p_data_width-1) downto 0);
-		i_we 			: in 	std_logic;
-		i_addr 		: in 	std_logic_vector((p_add_width-1) downto 0);
-		i_addw 		: in 	std_logic_vector((p_add_width-1) downto 0);
+		
+		-- pino LD do logisim
+			-- se 1 → bota na saida o que ta no endereço
+			-- se 0 → escreve no endereço o que ta na entrada de dados
+		i_wr_ram 	: in 	std_logic;
+		
+		-- pino SEL do logisim
+			-- Se a ram ta ligada
+		i_en_ram 	: in 	std_logic;
+		
+		-- apenas uma entrada de endereço, pois estou usando o mesmo modelo do logisim
+		i_add 		: in 	std_logic_vector((p_add_width-1) downto 0);
+		
+		-- botão de reset da ram
+		i_clr			: in 	std_logic;
+		
+		-- saida
 		o_data 		: out std_logic_vector((p_data_width-1) downto 0)
 	);
 end RAM;
 
 architecture Behavioral of RAM is
-	type mem_type is array(i_addr'range) of std_logic_vector(i_data'range);
+	type mem_type is array(i_add'range) of std_logic_vector(i_data'range);
 	signal w_memoria_ram : mem_type;
 begin
 	
-	-- Process da RAM
-	-- isso aqui ta diferente do que ele mostra no grafico do pdf...
-		-- La ele da apenas uma entrada de endereço
-		-- ele enfiou o wr no cu?? e we == en???
+	
 	process(i_clk)
 	begin
 		if(rising_edge(i_clk)) then
-			if(i_we = '1') then
-				w_memoria_ram(conv_integer(i_addw)) <= i_data;
-			end if
-			o_data <= w_memoria_ram(conv_integer(i_addr))
-			end if
+			if(i_en_ram = '1') then
+				if (i_wr_ram = '1') then
+					o_data <= w_memoria_ram(conv_integer(i_add));
+				else
+					w_memoria_ram(conv_integer(i_add)) <= i_data;
+				end if;
+			end if;
+		end if;
+		
+	
 	end process;
 	
 end Behavioral;
